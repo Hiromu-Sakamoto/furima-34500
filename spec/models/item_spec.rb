81897d86647ddfff0b5b2f037b7675492b8aa53d
Hiromu-Sakamoto <s_166_pc@yahoo.co.jp>
2021-03-09 11:33:20 +0900

    商品出品機能実装_修正3回目

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -68,7 +68,7 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include('Price is not a number')
       end
       it 'priceが半角数字以外では登録できない' do
-        @item.price = ３００
+        @item.price = '３００'
         @item.valid?
         expect(@item.errors.full_messages).to include('Price is not a number')
       end
